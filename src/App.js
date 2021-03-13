c6269ea9a172a3afee4c6adcfc1a1437bdc97fab
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-03-14 00:23:52 +0600

    reducer and actions are now separated than main files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/header";
 import Home from "./components/home";
 import Login from "./components/login";
+import authreducer from "./context/login-context/auth-reducer";
 
 export const AuthContext = React.createContext();
 
@@ -12,34 +13,34 @@ const initialState = {
   token: null,
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(action.payload.user));
-      localStorage.setItem("token", JSON.stringify(action.payload.token));
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "LOGIN":
+//       localStorage.setItem("user", JSON.stringify(action.payload.user));
+//       localStorage.setItem("token", JSON.stringify(action.payload.token));
 
-      return {
-        ...state,
-        isAuthnticated: true,
-        user: action.payload.user,
-        token: action.payload.token,
-      };
+//       return {
+//         ...state,
+//         isAuthnticated: true,
+//         user: action.payload.user,
+//         token: action.payload.token,
+//       };
 
-    case "LOGOUT":
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthnticated: false,
-        user: null,
-      };
+//     case "LOGOUT":
+//       localStorage.clear();
+//       return {
+//         ...state,
+//         isAuthnticated: false,
+//         user: null,
+//       };
 
-    default:
-      return state;
-  }
-};
+//     default:
+//       return state;
+//   }
+// };
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [state, dispatch] = React.useReducer(authreducer, initialState);
   return (
     <AuthContext.Provider
       value={{
---
+++ src/context/login-context/auth-reducer.js
@@ -0,0 +1,29 @@
+import { LOGIN, LOGOUT } from "./auth-action";
+
+const authreducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+
+      return {
+        ...state,
+        isAuthnticated: true,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthnticated: false,
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default authreducer;
