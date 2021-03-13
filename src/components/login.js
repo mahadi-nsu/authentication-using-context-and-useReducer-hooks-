cabc61982270f757c71537ee09a6bd589a79f428
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-03-13 21:00:45 +0600

    login added successfully

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,119 @@
 import React from "react";
-import logo from "../logo.svg";
 import { AuthContext } from "../App";
 export const Login = () => {
+  const { dispatch } = React.useContext(AuthContext);
+
+  const initialState = {
+    email: "",
+    password: "",
+    isSubmitting: false,
+    errorMessage: null,
+  };
+
+  const [data, setData] = React.useState(initialState);
+  const handleInputChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+  // const handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   setData({
+  //     ...data,
+  //     isSubmitting: true,
+  //     errorMessage: null,
+  //   });
+
+  //   fetch("https://hookedbe.herokuapp.com/api/login", {
+  //     method: "post",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       username: data.email,
+  //       password: data.password,
+  //     }),
+  //   });
+  // };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setData({
+      ...data,
+      isSubmitting: true,
+      errorMessage: null,
+    });
+    fetch("https://hookedbe.herokuapp.com/api/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: data.email,
+        password: data.password,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw res;
+      })
+      .then((resJson) => {
+        dispatch({
+          type: "LOGIN",
+          payload: resJson,
+        });
+      })
+      .catch((error) => {
+        setData({
+          ...data,
+          isSubmitting: false,
+          errorMessage: error.message || error.statusText,
+        });
+      });
+  };
+
+  console.log(data.password);
+  console.log(data.email);
+
   return (
     <div className="login-container">
       <div className="card">
-        <div className="container">login</div>
+        <div className="container">
+          <form>
+            <h1>Login</h1>
+
+            <label htmlFor="email">
+              Email Address
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={data.email}
+                onChange={handleInputChange}
+              />
+            </label>
+
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={data.password}
+                onChange={handleInputChange}
+              />
+            </label>
+
+            {data.errorMessage && (
+              <span className="form-error">{data.errorMessage}</span>
+            )}
+
+            <button disabled={data.isSubmitting} onClick={handleFormSubmit}>
+              {data.isSubmitting ? "Loading..." : "Login"}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
