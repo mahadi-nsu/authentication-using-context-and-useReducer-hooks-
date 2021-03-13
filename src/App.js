96054a7c798519a1fa1f6c94d242ea7e01751763
mahadi-nsu <mahadi.hassan@northsouth.edu>
2021-03-13 12:28:00 +0600

    auth reducer added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Home from "./components/home";
+import Login from "./components/login";
+
+export const AuthContext = React.createContext();
+
+const initialState = {
+  isAuthnticated: false,
+  user: null,
+  token: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", JSON.stringify(action.payload.token));
+
+      return {
+        ...state,
+        isAuthnticated: true,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthnticated: false,
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      <div>
+        {!state.isAuthnticated ? (
+          <>
+            <Header />
+            <Login />
+          </>
+        ) : (
+          <Home />
+        )}
+      </div>
+    </AuthContext.Provider>
   );
 }
-
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+export const Header = () => {
+  return (
+    <nav id="navigation">
+      <h1 href="#" className="logo">
+        Header
+      </h1>
+    </nav>
+  );
+};
+export default Header;
